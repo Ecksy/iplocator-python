1e9c0a912be639ab147ac18ffbd331718f19644c
Ecksy <38439556+Ecksy@users.noreply.github.com>
2018-09-23 16:55:46 -0500

    Add files via upload

---
+++ IPLocator.py
@@ -0,0 +1,46 @@
+import bs4 as bs
+import urllib.request
+
+#print header out: ''' prints multiple lines
+header = '''
+Ip Geolocation Tool
+By: Ecksy
+------------------------------------'''
+print(header)
+
+#read IP address(s) from input or file
+#ip = "argument from input"
+
+#get ip-api url
+#sauce = urllib.request.urlopen('http://ip-api.com/xml/'ip).read()
+sauce = urllib.request.urlopen('http://ip-api.com/xml/24.35.138.104').read()
+
+#create BeautifulSoup object
+soup = bs.BeautifulSoup(sauce, 'xml')
+
+#print the query result without the xml tags
+print('[!] IP               :  24.35.138.104')
+print('[!] Query Status     : ', soup.status.text)
+print('[!] Error Message    : ', soup.message)
+print('------------------------------------')
+print('[+] Country/Code     : ', soup.country.text, '-', soup.countryCode.text)
+print('[+] Region/Code      : ', soup.regionName.text, '-', soup.region.text)
+print('[+] City             : ', soup.city.text)
+print('[+] Zip              : ', soup.zip.text)
+print('[+] Geo              :  Lat:', soup.lat.text, '- Long:', soup.lon.text)
+print('[+] Paste to Maps    : ', soup.lat.text, ', ', soup.lon.text)
+print('[+] Timezone         : ', soup.timezone.text)
+print('[+] ISP              : ', soup.isp.text)
+print('[+] Organization     : ', soup.org.text)
+# print('[+] AS Number/Name   : ', soup.as.text)
+print('[+] https://maps.google.com/maps/place/', soup.status.text)
+
+#Usage: ./iplocator.pl [host] [ip] [domain]
+       # perl iplocator.pl [host] [ip] [domain]
+#
+# Ex:  ./iplocator.pl  www.google.com
+#      ./iplocator.pl  216.58.210.206
+#
+# Usage Limit: The ip-api.com system will automatically ban any IP addresses doing over 150 requests per minute.
+#
+# If your IP was banned, visit http://ip-api.com/docs/unban
